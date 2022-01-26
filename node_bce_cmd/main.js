ae60afcfbea40c9ea6dab1b1fffa41cfd42089e0
zhangbiwu <cherishman2005@163.com>
2022-01-26 16:16:38 +0800

    Add files via upload

--- node_bce_cmd/main.js
+++ node_bce_cmd/main.js
@@ -1,4 +1,5 @@
 const client = require('./bce_client');
+const fs = require('fs');
 
 // 以file形式上传
 let key = 'dataset/zhangbiwu/package.js';
@@ -17,5 +18,11 @@ client.putObjectFromString(key, JSON.stringify(data));
 // 以buffer形式上传
 let object = 'dataset/zhangbiwu/yylive123.txt';
 let buffer = new Buffer('hello world'); 
+client.putObject(object, buffer)
+
+
+let bf = fs.readFileSync('./seaweedfs.png');
+object = 'dataset/zhangbiwu/seaweedfs_test.png';
+buffer = Buffer.from(bf); 
 client.putObject(object, buffer)
 
